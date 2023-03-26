f67e7e542f9168bf4eeff1013c4f0a5d86e33abd
Nitish Mahawar <nitishmahawar112@gmail.com>
2023-03-26 13:07:12 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import React from "react";
-import Featured from "./components/Featured";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import New from "./components/New";
 
 const App = () => {
-  return (
-    <div className="min-h-screen px-4 md:px-12 md:py-8 lg:py-10 lg:px-20">
-      <Navbar />
-      <div className="grid md:grid-cols-3 gap-6">
-        <Hero />
-        <New />
-      </div>
-      <Featured />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,17 +3,8 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        "primary-1": "hsl(35, 77%, 62%)",
-        "primary-2": "hsl(5, 85%, 63%)",
-        "neutral-1": "hsl(36, 100%, 99%)",
-        "neutral-2": "hsl(233, 8%, 79%)",
-        "neutral-3": "hsl(236, 13%, 42%)",
-        "neutral-4": "hsl(240, 100%, 5%)",
-      },
-      fontFamily: {
-        sans: ["Inter", "sans-serif"],
-      },
+      colors: {},
+      fontFamily: {},
     },
   },
   plugins: [],
