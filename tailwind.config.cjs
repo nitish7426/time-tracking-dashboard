55bc25fe97be77d398347948146900f8aff20dc8
Nitish Mahawar <nitishmahawar112@gmail.com>
2023-04-07 18:54:24 +0530

    first commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,16 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {},
-      fontFamily: {},
+      colors: {
+        "primary-100": "hsl(263, 55%, 52%)",
+        "primary-200": "hsl(217, 19%, 35%)",
+        "primary-300": "hsl(219, 29%, 14%)",
+        "neutral-100": "hsl(0, 0%, 81%)",
+        "neutral-200": "hsl(210, 46%, 95%)",
+      },
+      fontFamily: {
+        sans: ["Barlow Semi Condensed", "sans-serif"],
+      },
     },
   },
   plugins: [],
